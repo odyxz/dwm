60398fcfebfbfa0e25e7229c594d4bd3ca485c87
User <user@example.com>
2021-07-19 20:47:52 +0300

    initial

--- config.h
+++ config.h
@@ -1,30 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char *fonts[]          = { "xos4 Terminus:size=10" };
+static const char dmenufont[]       = "xos4 Terminus:size=10";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#18381a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  "#9ccc9c"  },
+	[SchemeTitle]  = { col_gray4, col_gray2,  col_gray2  },
+	[SchemeTitleSel]  = { "#FFFFFF", col_cyan,  col_gray2  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1:main", "2:term", "3:web", "4:dev", "5:docs", "6:irc", "7:file", "8:misc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -32,8 +36,16 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
+	{ "Chromium",		NULL,       NULL,       1 << 2,       0,           -1 },
+//	{ "Zathura",  		NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "code-oss",           NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Emacs",    		NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "TelegramDesktop",    NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "discord",    	NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Pcmanfm",  		NULL,	    NULL,       1 << 6,       0,           -1 },
+	{ "KeePassXC",  	NULL,	    NULL,       1 << 7,       0,           -1 },
+//	{ "Firefox",  		NULL, 	    NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -65,7 +77,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -77,8 +89,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", "search:", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *slockcmd[] = { "slock",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -87,15 +100,18 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+        { MODKEY,                       XK_Left,   focusstack,     {.i = +1 } },
+        { MODKEY,                       XK_Right,  focusstack,     {.i = -1 } },
+	{ MODKEY,			XK_space,  focusmaster,    {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
-	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
-	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
+	{ Mod1Mask|ShiftMask,           XK_h,      setmfact,       {.f = -0.05} },
+	{ Mod1Mask|ShiftMask,           XK_l,      setmfact,       {.f = +0.05} },
+	{ Mod1Mask|ShiftMask,           XK_j,      setcfact,       {.f = +0.25} },
+	{ Mod1Mask|ShiftMask,           XK_k,      setcfact,       {.f = -0.25} },
+	{ Mod1Mask|ShiftMask,           XK_o,      setcfact,       {.f =  0.00} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
+	/*{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
 	{ MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
@@ -108,9 +124,9 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },*/
+	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -125,6 +141,18 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_l,	   spawn,	   {.v = slockcmd } },
+	{ MODKEY|ControlMask,           XK_Right,           	view_adjacent,  { .i = +1 } },
+	{ MODKEY|ControlMask,           XK_Left,           	view_adjacent,  { .i = -1 } },
+	{ 0,                            XK_Print,  spawn,          SHCMD("maim ~/Pictures/Screenshots/$(date +%Y-%m-%d-%H%M%S)_scrot.png ; notify-send 'Screen captured'") },
+	{ ShiftMask,                    XK_Print,  spawn,          SHCMD("maim -s ~/Pictures/Screenshots/$(date +%Y-%m-%d-%H%M%S)_scrot.png -u ; notify-send 'Screen captured'") },
+	{ MODKEY,                            XK_Print,  spawn,          SHCMD("maim | xclip -selection clipboard -t image/png ; notify-send 'Screen captured and copied to clipboard'") },
+	{ MODKEY|ShiftMask,                    XK_Print,  spawn,          SHCMD("maim -s -u | xclip -selection clipboard -t image/png ; notify-send 'Screen captured and copied to clipboard'") },
+        { 0,              XF86XK_AudioRaiseVolume, spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +1000 ; notify-send --app-name=volume_notifiers Volume: $(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( 1 )) | tail -n 1 | sed -e 's,.* \\([0-9][0-9]*\\)%.*,\\1,')%") },
+        { 0,              XF86XK_AudioLowerVolume, spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -1000 ; notify-send --app-name=volume_notifiers Volume: $(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( 1 )) | tail -n 1 | sed -e 's,.* \\([0-9][0-9]*\\)%.*,\\1,' )%")  },
+        { 0,              XF86XK_AudioMute,        spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle ; notify-send 'Volume: Mute'") },
+	{ 0,              XF86XK_MonBrightnessUp,  spawn,          SHCMD("xbacklight -inc 10; notify-send --app-name=brightness_notifiers Brightness: $(xbacklight -get | awk '{print int($1)}')%") },
+	{ 0,              XF86XK_MonBrightnessDown,spawn,          SHCMD("xbacklight -dec 10; notify-send --app-name=brightness_notifiers Brightness: $(xbacklight -get | awk '{print int($1)}')%") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
