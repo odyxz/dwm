6a1ce6ec094438288ff4e97ce5095ab4625fa514
User <user@example.com>
2021-07-21 15:58:40 +0300

    change slock to i3lock-fancy

--- config.h
+++ config.h
@@ -91,7 +91,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", "search:", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *slockcmd[] = { "slock",  NULL };
+static const char *slockcmd[] = { "i3lock-fancy",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
