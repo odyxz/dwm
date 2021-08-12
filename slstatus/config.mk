bdae5d8becd2b816bb9cebc8c360188a13c31e35
User <user@example.com>
2021-08-12 09:13:38 +0300

    switch to clang and lto

--- slstatus/config.mk
+++ slstatus/config.mk
@@ -12,11 +12,11 @@ X11LIB = /usr/X11R6/lib
 
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
+CFLAGS   = -std=c99 -pedantic -Wall -Wextra -O3
 LDFLAGS  = -L$(X11LIB) -s
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm
 LDLIBS   = -lX11
 
 # compiler and linker
-CC = cc
+CC = clang -flto
